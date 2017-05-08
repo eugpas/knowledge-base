c1de517313665f814bbd218721d5b510bf3b924b
ypasch <tekilla.com@gmail.com>
2017-05-08 19:24:26 +0300

    dev branch initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Wot stats app.';
+  title = 'Wot-stats app running...';
 }

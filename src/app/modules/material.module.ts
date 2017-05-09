38c1c0f40e8c33b42b1f493886180eb0d1294ef9
ypasch <tekilla.com@gmail.com>
2017-05-09 08:35:31 +0300

    adding ng material ot the project

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class MaterialModule { }
